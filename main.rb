cebedd7c7b3d7be9beb54f965d3c3f7f71d3c873
Daniel Smilansky <dsmilansky@gmail.com>
2013-05-24 12:01:47 -0700

    Initial Setup

--- main.rb
+++ main.rb
@@ -3,24 +3,66 @@ require 'sinatra'
 
 set :sessions, true
 
-session = {}
+helpers do
+  def to_image(card)
+    
+    card.each do |holder|
+  end
+ end 
+
+  def calc_value(cards)
+    sum = 0
+    arr = cards.map{|e| e[1]}
+
+      arr.each do |value|
+        if value == 'A'
+          sum += 11
+        elsif value.to_i == 0
+          sum += 10
+        else
+          sum += value.to_i
+        end
+      end      
+    arr.select {|e| e == 'A'}.count.times do 
+      sum -=10 if sum > 21
+    end
+    sum
+  end  
+end  
 
 get '/' do 
-  if session[:player] 
-    "Welcome to Blackjack, #{session[:player]}!"
-  else
-    redirect '/form'
-  end
+  redirect '/name'
 end
 
-get '/form' do
-  erb :form
+get '/name' do
+  erb :name
 end  
 
-post '/login' do
- session[:player] = params['player']
+post '/name' do
+  session[:player] = params['player']
+  if session[:player].nil? || session[:player].empty?
+    @error = "You must input a name"
+    erb :name
+  else
+    redirect '/game'
+  end
 end  
 
-# get '/layout' do
-#   erb :layout
-# end
+get '/game' do
+  suits = ['H', 'D', 'S', 'C']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle
+
+  session[:playercards] = []
+  session[:dealercards] = []
+  session[:playercards] << session[:deck].pop
+  session[:dealercards] << session[:deck].pop
+  session[:playercards] << session[:deck].pop
+  session[:dealercards] << session[:deck].pop
+
+  session[:playertotal] = calc_value(session[:playercards])
+  session[:dealertotal] = calc_value(session[:dealercards])
+erb :game
+end  
+
+
